fd6d1bd8700819b8e0f4f426eee8b7e4f450ff98
JakeDrohman <jkdrohman@gmail.com>
2017-01-24 13:51:06 -0600

    seeded roles

--- Concordia_Web_App/Startup.cs
+++ Concordia_Web_App/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using Concordia_Web_App.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Concordia_Web_App.Startup))]
@@ -9,6 +12,43 @@ namespace Concordia_Web_App
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+            }
+            if (!roleManager.RoleExists("Professor"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Professor";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Advisor"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Advisor";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Data_Entry"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "_Entry";
+                roleManager.Create(role);
+            }
         }
     }
 }
